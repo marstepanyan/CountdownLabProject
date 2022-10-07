7ff26fe3658ff8133e3827b14f152ac6e3c7cf95
marstepanyan <mariam.stepanyan.02@gmail.com>
2022-10-07 23:11:54 +0400

    added git ignore

---
+++ numbers.py
@@ -0,0 +1,52 @@
+# num = input('enter number: ')
+
+
+def ten_segment_num(number):
+    rows = [''] * 3
+
+    for i, digit in enumerate(number):
+        if digit == '0':
+            rows[0] += ' __ '
+            rows[1] += '|  |'
+            rows[2] += '|__|'
+        elif digit == '1':
+            rows[0] += '    '
+            rows[1] += '   |'
+            rows[2] += '   |'
+        elif digit == '2':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += '|__ '
+        elif digit == '3':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += ' __|'
+        elif digit == '4':
+            rows[0] += '    '
+            rows[1] += '|__|'
+            rows[2] += '   |'
+        elif digit == '5':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += ' __|'
+        elif digit == '6':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += '|__|'
+        elif digit == '7':
+            rows[0] += ' __ '
+            rows[1] += '   |'
+            rows[2] += '   |'
+        elif digit == '8':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += '|__|'
+        elif digit == '9':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += ' __|'
+
+    return '\n'.join(rows)
+
+
+print(ten_segment_num(num))
